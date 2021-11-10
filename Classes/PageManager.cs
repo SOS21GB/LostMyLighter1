ccae042ea48bbf63162e2c832b868cfc6d94220c
davidKael <70914882+davidKael@users.noreply.github.com>
2021-11-10 20:51:09 +0100

    Ny klass "PageManager", inhåller tomma funktioner där vi sedan kan lägga i flödet för varje sida.. har påbörjat med MainMenu()

---
+++ Classes/PageManager.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostMyLighter.Classes
+{
+    static class PageManager
+    {
+        public static void StartApp()
+        {
+            //här lägger vi sedan login/create user först
+            MainMenu();
+        }
+
+        /// <summary>
+        /// Component to be placed on top of each page, containing the page's title
+        /// </summary>
+        /// <param name="title"></param>
+        static void PageHeader(string title)
+        {
+            Console.Clear();
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine(title);
+            Console.WriteLine("------------------------------------------------");
+        }
+
+        /// <summary>
+        /// Call this method to let the user know that the input was not accepted
+        /// </summary>
+        static void ErrorMessage()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Input not valid");
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        static void LogIn()
+        {
+            string titel = "Log in";
+            PageHeader(titel);
+            //här lägger vi flödet för inlogg
+        }
+
+        static void CreateUser()
+        {
+            string titel = "Create User";
+            PageHeader(titel);
+            //här lägger vi flödet för skapandet av ny användare
+        }
+
+        static void MainMenu()
+        {
+            string title = "Main Menu";
+
+            while (true)
+            {
+                PageHeader(title);
+                Console.WriteLine();
+                Console.WriteLine("1. Search marschalls.");
+                Console.WriteLine("2. Add marschall.");
+                Console.WriteLine("3. View profile.");
+                Console.WriteLine("4. Log out.");
+                Console.WriteLine();
+                Console.WriteLine("------------------------------------------------");
+
+                if (int.TryParse(Console.ReadLine(), out int userInput))
+                {
+                    switch (userInput)
+                    {
+                        case 1:
+                            SearchMarschalls();
+                            return;
+                        case 2:
+                            AddMarschall();
+                            return;
+                        case 3:
+                            ViewProfile();
+                            return;
+                        case 4:
+                            LogOut();
+                            return;
+                    }
+                }
+
+                PageHeader(title);
+                ErrorMessage();
+            }
+
+        }
+
+        static void ViewProfile()
+        {
+            string titel = "Profile";
+            PageHeader(titel);
+        }
+
+        static void SearchMarschalls()
+        { 
+            string titel = "Search";
+            PageHeader(titel);
+            //här lägger vi hela flödet för Search-sidan
+        }
+
+        static void AddMarschall()
+        {
+            string titel = "Add Marschall";
+            PageHeader(titel);
+            //här lägger vi hela flödet för AddMarschall-sidan
+        }
+
+        static void LogOut()
+        {
+            Console.Clear();
+            Console.WriteLine("*************Logging out******************");
+            //här loggas man ut och kommer tillbaka till inlogg/create user
+        }
+    }
+}
