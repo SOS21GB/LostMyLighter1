c6631adc4d987b262d42a13b9d91f62716126662
BondeJohan <johan.andersson3086@outlook.com>
2021-11-10 16:01:11 +0100

    First Upload

--- Classes/User.cs
+++ Classes/User.cs
@@ -10,7 +10,18 @@ namespace LostMyLighter.Classes
     {
         int _id;
         string _userName;
+        int _age;
+        string _adress;
+        int _numberOfSearchers;
+        int _numberOfMarchaller;
+        int _lostLighters;
+        
         public int Id { get { return _id; } }
         public string UserName { get { return _userName; } }
+        public int Age { get { return _age; } }
+        public string Adress { get { return _adress; } }
+        public int NumberOfSearchers { get { return _numberOfSearchers; } }
+        public int NumberOfMarchaller { get { return _numberOfMarchaller; } }
+        public int LostLighters { get { return _lostLighters; } }
     }
 }
